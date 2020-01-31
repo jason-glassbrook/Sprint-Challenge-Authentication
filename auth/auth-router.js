dc09a2185daee4ffc93f179f6d631eddef96e3ef
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-31 12:32:02 -0500

    implement /auth/register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,37 @@
 const router = require ('express').Router ()
 
+const bcrypt = require ('bcryptjs')
+const jwt = require ('jsonwebtoken')
+const { jwtSecret } = require ('../config/secrets.js')
+
+const Users = require ('../users/users-model.js')
+
 router.post ('/register', (req, res) => {
   // implement registration
+
+  const { username, password } = req.body
+
+  const user = {
+    username,
+    hash : bcrypt.hashSync (password, 10)
+  }
+
+  Users.push (user)
+  .then ((record) => {
+
+    res
+    .status (201)
+    .json (record)
+
+  })
+  .catch ((error) => {
+
+    res
+    .status (500)
+    .json (error)
+
+  })
+
 })
 
 router.post ('/login', (req, res) => {
