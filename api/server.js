4fb959032c7eaf9850c081451a169ec693c1489e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-31 11:31:52 -0500

    update code style -- OCD

--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require ('express')
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require ('../auth/authenticate-middleware.js')
+const authRouter = require ('../auth/auth-router.js')
+const jokesRouter = require ('../jokes/jokes-router.js')
 
-const server = express();
+const server = express ()
 
-server.use([
-  require('helmet') (),
-  require('cors') (),
-  require('morgan') ('dev'),
+server.use ([
+  require ('helmet') (),
+  require ('cors') (),
+  require ('morgan') ('dev'),
   express.json (),
-]);
+])
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use ('/api/auth', authRouter)
+server.use ('/api/jokes', authenticate, jokesRouter)
 
-module.exports = server;
+module.exports = server
