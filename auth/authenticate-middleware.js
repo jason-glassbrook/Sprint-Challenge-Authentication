49e2f1c86f661e9fef7581d6931f3174e7b76d5e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-31 13:40:19 -0500

    fill out authenticate

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,48 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status (401).json ({ you: 'shall not pass!' })
+module.exports = authenticate
+
+/**************************************/
+
+const jwt = require ('jsonwebtoken')
+const { jwtSecret } = require ('../config/secrets.js')
+
+const respondWithInvalidCredentials = require ('./respond-with-invalid-credentials')
+
+/**************************************/
+
+function authenticate (req, res, next) {
+
+  const token = req.headers.authorization
+
+  if (token) {
+
+    jwt.verify (token, jwtSecret, (error, decrypted) => {
+
+      if (error) {
+
+        respondWithInvalidCredentials () (req, res)
+
+      }
+      else {
+
+        req.user = {
+          _id : decrypted.id,
+          username : decrypted.id,
+        }
+
+        next ()
+
+      }
+
+    })
+
+  }
+  else {
+
+    respondWithInvalidCredentials () (req, res)
+
+  }
+
 }
