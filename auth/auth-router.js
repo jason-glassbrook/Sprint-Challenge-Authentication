8299e35c2d76ac67ad43a5c4c09aa62965cff439
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-31 12:51:21 -0500

    implement /auth/login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,7 @@
 const router = require ('express').Router ()
 
 const bcrypt = require ('bcryptjs')
-const jwt = require ('jsonwebtoken')
-const { jwtSecret } = require ('../config/secrets.js')
+const signToken = require ('./signToken')
 
 const Users = require ('../users/users-model.js')
 
@@ -40,6 +39,43 @@ router.post ('/register', (req, res) => {
 router.post ('/login', (req, res) => {
 
   // implement login
+
+  const { username, password } = req.body
+
+  Users.findBy ({ username }, '*')
+  .then (([ user ]) => {
+
+    if (user && bcrypt.compareSync (password, user.hash)) {
+
+      const token = signToken (user)
+
+      res
+      .status (200)
+      .json ({
+        message : `Welcome, ${user.username}. Take this.`,
+        token,
+      })
+
+    }
+    else {
+
+      res
+      .status (401)
+      .json ({
+        error : {
+          message : `invalid credentials`,
+        },
+      })
+    }
+
+  })
+  .catch ((error) => {
+
+    res
+    .status (500)
+    .json (error)
+
+  })
 
   return
 
